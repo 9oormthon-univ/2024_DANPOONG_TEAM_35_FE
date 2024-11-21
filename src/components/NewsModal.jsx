e6a7a886f748424e9c74edc26a0b689069c5e674
yungan9 <geyun105@naver.com>
2024-11-22 03:57:49 +0900

    design : 이슈 리스트 디자인 추가

--- src/components/NewsModal.jsx
+++ src/components/NewsModal.jsx
@@ -29,7 +29,13 @@ export default function NewsModal() {
             </DateBox>
           </TopContainer>
           <MainContainer>
-            <NewsList />
+            <NewsList>
+              <NewsListTitle>
+                <p>제목</p>
+                <Checkbox />
+              </NewsListTitle>
+              <NewsListContent>내용</NewsListContent>
+            </NewsList>
             <NewsList />
             <NewsList />
             <NewsList />
@@ -147,4 +153,42 @@ const MainContainer = styled.div`
   height: 400px;
   border-radius: 8px;
   overflow: auto;
+`;
+
+const NewsList = styled.div`
+  padding: 8px;
+  margin-bottom: 8px;
+  flex: 1;
+  height: 80px;
+  border: 1px solid #717171;
+  border-radius: 8px;
+  background-color: white;
+  font-size: 14px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+
+const NewsListTitle = styled.div`
+  flex: 1;
+  display: flex;
+  font-size: 18px;
+  font-weight: 600;
+  padding: 12px;
+  color: black;
+  justify-content: space-between;
+`;
+
+const NewsListContent = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  padding-left: 12px;
+  padding-bottom: 12px;
+  color: #828282;
+`;
+
+const Checkbox = styled.input.attrs({ type: "checkbox" })`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
 `;
