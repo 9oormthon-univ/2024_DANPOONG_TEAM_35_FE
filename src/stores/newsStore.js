579edd10b76213916955566a673f8172249326ae
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-11-23 20:01:56 +0900

    feat : 선택된 시트 local storage에서 상태 저장

--- src/stores/newsStore.js
+++ src/stores/newsStore.js
@@ -1,31 +1,28 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useNewsCardStore = create((set) => ({
-  selectedNewsCards: [],
+const useNewsStore = create(
+  persist(
+    (set) => ({
+      selectedNewsCards: [],
 
-  addSelectedNewsCard: (card) =>
-    set((state) => ({
-      selectedNewsCards: [...state.selectedNewsCards, card],
-    })),
-
-  removeSelectedNewsCard: (id) =>
-    set((state) => ({
-      selectedNewsCards: state.selectedNewsCards.filter(
-        (card) => card.id !== id
-      ),
-    })),
-
-  toggleSelectedNewsCard: (card) =>
-    set((state) => {
-      const exists = state.selectedNewsCards.find(
-        (selected) => selected.id === card.id
-      );
-      return {
-        selectedNewsCards: exists
-          ? state.selectedNewsCards.filter((s) => s.id !== card.id)
-          : [...state.selectedNewsCards, card],
-      };
+      toggleSelectedNewsCard: (card) =>
+        set((state) => {
+          const exists = state.selectedNewsCards.find(
+            (selected) => selected.id === card.id
+          );
+          return {
+            selectedNewsCards: exists
+              ? state.selectedNewsCards.filter((s) => s.id !== card.id)
+              : [...state.selectedNewsCards, card],
+          };
+        }),
     }),
-}));
+    {
+      name: "news-storage",
+      partialize: (state) => ({ selectedNewsCards: state.selectedNewsCards }),
+    }
+  )
+);
 
-export default useNewsCardStore;
+export default useNewsStore;
